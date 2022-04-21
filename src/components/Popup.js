be5c0eb6fcc82a834d5e998c5c16c7b82b019d71
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2022-04-21 10:43:52 +0100

    fix: typo in popup component

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,24 +1,39 @@
 import React, {useEffect} from 'react'
-import { checkWin } from '../helpers/helpers'
 
-const Popup = ({wrongLetters, correctedLetters, selectedWord, setPlayable, playAgain}) => {
+const Popup = ({wrongLetters, correctLetters, selectedWord, setPlayable, playAgain}) => {
   let finalMessage = ''
   let finalMessageRevealWord = ''
   let playable = true;
 
-  if (checkWin(correctedLetters, wrongLetters, selectedWord)=== 'win') {
+  if (checkWin(correctLetters, wrongLetters, selectedWord)=== 'win') {
       finalMessage = 'Congratulations! You won.'
       playable = false
-  } else if (checkWin(correctedLetters, wrongLetters, selectedWord)=== 'lose') {
+  } else if (checkWin(correctLetters, wrongLetters, selectedWord)=== 'lose') {
     finalMessage = 'Unfortunately! You lost.'
     playable = false
     finalMessageRevealWord = `... the word was ${selectedWord}`
   }
 
+  function checkWin(correct, wrong, word) {
+    let status = 'win';
+  
+    // Check for win
+    word.split('').forEach(letter => {
+      if(!correct.includes(letter)){
+        status = '';
+      }
+    });
+    
+    // Check for lose
+    if(wrong.length === 6) status = 'lose';
+  
+    return status
+  }
+
   useEffect(() => setPlayable(playable))
 
     return (
-    <div className="popup-container" style={finalMessage !== '' ? {display: flex} : {}} >
+    <div className="popup-container" style={finalMessage !== '' ? {display: 'flex'} : {}} >
       <div className="popup">
         <h2 >{finalMessage}</h2>
         <h3 >{finalMessageRevealWord}</h3>
