f0a0ad4a1fe0516820f8ad36ae89863bd9ef08cd
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2022-04-21 08:09:04 +0100

    added functions to App components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,71 @@
+import React, { useState, useEffect } from 'react';
 import Figure from './components/Figure';
 import WrongLetters from './components/WrongLetters';
-import Word from './components/Word'
-import './App.css';
+import Word from './components/Word';
 import Popup from './components/Popup';
 import Notification from './components/Notification';
-import { useState } from 'react';
+import { showNotification as show, checkWin } from './helpers/helpers';
+
+import './App.css';
 
 const words = ['application', 'programming', 'interface', 'wizard'];
-
 let selectedWord = words[Math.floor(Math.random() * words.length)];
-
-let playable = true;
-const correctLetters = [];
-const wrongLetters = [];
-
 
 function App() {
   const [playable, setPlayable] = useState(true);
   const [correctLetters, setCorrectLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const handleKeydown = event => {
+      const { key, keyCode } = event;
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters(currentLetters => [...currentLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+    }
+    window.addEventListener('keydown', handleKeydown);
+
+    return () => window.removeEventListener('keydown', handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
+
+  function playAgain() {
+    setPlayable(true);
+
+    // Empty Arrays
+    setCorrectLetters([]);
+    setWrongLetters([]);
+
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
 
   return (
     <>
-      <h1>Hangman</h1>
-      <p>Find the hidden word - Enter a letter </p>
-
-      <div className='game-container'>
-        <Figure />
-        <WrongLetters />
-        <Word selectedWord={selectedWord} correctLetters = {correctLetters}/>
+      <>
+        <h1>Hangman</h1>
+        <p>Find the hidden word - Enter a letter</p>
+      </>
+      <div className="game-container">
+        <Figure wrongLetters={wrongLetters} />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
       </div>
-      <Popup />
-      <Notification />
+      <Popup correctLetters={correctLetters} wrongLetters={wrongLetters} selectedWord={selectedWord} setPlayable={setPlayable} playAgain={playAgain} />
+      <Notification showNotification={showNotification} />
     </>
   );
 }
